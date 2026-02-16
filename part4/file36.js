1549ccf331d8752c4891ab7b64483257d572342d
suryadwd <surajdwivedi644@gmail.com>
2026-02-16 11:27:29 +0530

    added

---
+++ part4/file36.js
@@ -0,0 +1,10 @@
+let num = 10.75
+// console.log(Math.floor(num))  // neeche
+// console.log(Math.ceil(num))  // upar
+// console.log(Math.round(num))  // round
+// console.log(num.toFixed(1))  // roundoff by alter  after . 
+// console.log(isNaN(num))         // true if not a number vicevera
+// console.log(Math.random())       // random number between 0 1 
+// console.log(Math.max(5,7,1,4))     // max value in given
+// console.log(Math.min(5,7,1,4))     //min value 
+console.log(Math.pow(10,2))  // gives power
