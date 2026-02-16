effe221e221561fcbc06f288f065a21cdde539ea
suryadwd <surajdwivedi644@gmail.com>
2026-02-16 11:13:47 +0530

    added

--- part4/file35.js
+++ part4/file35.js
@@ -1,8 +1,16 @@
-const names = ["Surya", "Ajay", "akku"]
-
+// const names = ["Surya", "Ajay", "akku"]
+// const names = new Array("surya", "anuj", "rishi", "akku")
+const nums = [6,4,7,723,76,3,76]
 // names.push("Rishi") //  add rishi at last
 // names.pop() // remove lst
 // names.shift()  // remove fst
-names.unshift("Surya") // add surya at first 
+// names.unshift("Suraj") // add surya at first 
 // shift and unshift are slow as reaarangement happens in array while push and pop are faster
-console.log(names)
+// console.log(names)
+// console.log(names.sort()) //in asc in txt only
+// console.log(names.reverse())  
+
+// console.log(nums.sort((a, b) => a - b)) // asc  in num
+// console.log(nums.sort((a, b) => b - a)) // desc  in num
+console.log(nums.indexOf(76))   
+console.log(nums.lastIndexOf(76))
