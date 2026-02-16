d907b94fe205d1a6b35718152c3fb0aaf31d4900
suryadwd <surajdwivedi644@gmail.com>
2026-02-16 10:56:46 +0530

    added

---
+++ part4/file35.js
@@ -0,0 +1,8 @@
+const names = ["Surya", "Ajay", "akku"]
+
+// names.push("Rishi") //  add rishi at last
+// names.pop() // remove lst
+// names.shift()  // remove fst
+names.unshift("Surya") // add surya at first 
+// shift and unshift are slow as reaarangement happens in array while push and pop are faster
+console.log(names)
