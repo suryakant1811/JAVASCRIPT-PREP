a20bf5523e19dc22df5268b62d96eca3e087e257
suryadwd <surajdwivedi644@gmail.com>
2026-02-17 10:21:25 +0530

    promises

--- part5/file41.js
+++ part5/file41.js
@@ -1,20 +1,47 @@
-function f1(args) {
-  return new Promise((resolve, reject) => {
+// function f1(args) {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       if(args % 2 == 0) resolve(`${args} is a even number`)
+//       else reject("Odd Number not allowed")
+//     }, 2000);
+//   });
+// }
+
+// async function k() {
+//   let num = Math.round(Math.random()*10)
+//   try {
+//     const result = await f1(num);
+//     console.log(result)
+//     console.log("Programm executed successfully")
+//   } catch (error) {
+//     console.log("Program rejected: ", error)
+//   }
+// }
+// k();
+
+function getStudentInfo(){
+  return new Promise ((resolve, reject)=>
     setTimeout(() => {
-      if(args % 2 == 0) resolve(`${args} is a even number`)
-      else reject("Odd Number not allowed")
-    }, 2000);
-  });
+    const info = {
+    name: "surya",
+    email: "surya@gmil.com",
+    age: 21
+    }
+    resolve(info)
+  }, 2000)
+)
 }
 
-async function k() {
-  let num = Math.round(Math.random()*10)
+function displayInfo(result){
+  console.log(result.name)
+}
+
+async function main() {
   try {
-    const result = await f1(num);
-    console.log(result)
-    console.log("Programm executed successfully")
+    const result = await getStudentInfo()
+    displayInfo(result)
   } catch (error) {
-    console.log("Program rejected: ", error)
+    console.log("Error", error)
   }
 }
-k();
+main()
