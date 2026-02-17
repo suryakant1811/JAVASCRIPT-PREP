7c5c8ef8332187eed1a75d48f0916e4f100068a8
suryadwd <surajdwivedi644@gmail.com>
2026-02-17 09:50:47 +0530

    promises

--- part5/file41.js
+++ part5/file41.js
@@ -1,15 +1,17 @@
 function f1(args) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      if(args % 2 == 0) resolve()
+      if(args % 2 == 0) resolve(`${args} is a even number`)
       else reject("Odd Number not allowed")
     }, 2000);
   });
 }
 
 async function k() {
+  let num = Math.round(Math.random()*10)
   try {
-    await f1(5);
+    const result = await f1(num);
+    console.log(result)
     console.log("Programm executed successfully")
   } catch (error) {
     console.log("Program rejected: ", error)
