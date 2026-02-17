c9f6979ca739f9e5727242b7ec7b15d3c5b7859b
suryadwd <surajdwivedi644@gmail.com>
2026-02-17 09:26:56 +0530

    async await

--- part5/file40.js
+++ part5/file40.js
@@ -1,10 +1,17 @@
-// Promissess
 function f1(){
-    console.log("Function f1 output")
-}
-function f2(){
-    console.log("Function f2 output")
+   return new Promise((resolve, reject) => {
+     setTimeout(()=>{console.log("this is f1 function")
+         resolve()
+     }, 2000)
+   })
 }
 
-f1()
-f2()
+function f2(){
+    console.log("this is f2 function")
+}
+
+async function k(){
+    await f1()
+    f2()
+}
+k()
